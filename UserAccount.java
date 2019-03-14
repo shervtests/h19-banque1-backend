a8187cc6f753fe9c1aa031e51f3d2a0d4b8fcc52
shervinantoine <46613577+shervinantoine@users.noreply.github.com>
2019-03-14 12:27:25 -0400

    Update userAccount & UserCreditCard pour le probleme avec /usersA et /usersU

---
+++ UserAccount.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.grokonez.jwtauthentication.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author smile
+ */
+@Entity
+@JsonIgnoreProperties({"transactions","user"})
+public class UserAccount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long Id;
+    private String accountno;
+    private double amount = 0.00;
+  
+    @JsonIgnoreProperties("userAccount")
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+     
+    @OneToMany(fetch = FetchType.LAZY,
+            cascade =  CascadeType.ALL,  mappedBy = "userAccount" )
+      @JsonIgnoreProperties("userAccount")
+    private Set<Transactions> transactions;
+      
+    public UserAccount(long Id, String accountno, double amount) {
+        this.Id = Id;
+        this.accountno = accountno;
+        this.amount = amount;
+    }
+
+    public UserAccount() {
+    }
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long Id) {
+        this.Id = Id;
+    }
+
+    public String getAccountno() {
+        return accountno;
+    }
+
+    public void setAccountno(String accountno) {
+        this.accountno = accountno;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Set<Transactions> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(Set<Transactions> transactions) {
+        this.transactions = transactions;
+    }
+
+ 
+    
+    
+}
